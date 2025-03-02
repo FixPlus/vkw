5781a1639358d6d07115a4400e5d7defa2960334
FixPlus <bushev.da@phystech.edu>
2025-03-03 02:55:46 +0300

    [feature] Prettify build and install

--- cmake/find_dependencies.cmake
+++ cmake/find_dependencies.cmake
@@ -11,12 +11,17 @@ else ()
 endif ()
 
 # Boost dependencies
-find_package(Boost 1.62 REQUIRED)
+find_package(Boost 1.81 REQUIRED)
 
 include_directories(${Boost_INCLUDE_DIRS})
 
 #Vulkan include dependencies
 
-find_package(Vulkan 1.3 REQUIRED)
+find_package(Vulkan 1.3 REQUIRED COMPONENTS SPIRV-Tools)
+
+# FIXME: this works only on linux now, we need some another way to get spirv-link
+add_library(SPIRV_LINK INTERFACE IMPORTED)
+target_link_options(SPIRV_LINK INTERFACE "-L$ENV{VULKAN_SDK}/lib")
+target_link_libraries(SPIRV_LINK INTERFACE SPIRV-Tools-link SPIRV-Tools-opt SPIRV-Tools)
 
 include_directories(${Vulkan_INCLUDE_DIRS})
